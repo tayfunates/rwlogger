061dd382954929b162d32ea0bb8c72fa8f817fb7
Tayfun Ateş <tayfun.ates@avcrtech.com>
2019-02-09 00:17:31 +0300

    Some getters and setters are added.

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -15,29 +15,26 @@
 
 namespace rw
 {
-    //Lock class which will be responsible from thread sync
-    
-    
     Logger::Logger()
     {
         m_path = "";
         m_enabled = true;
-        m_logLevel = LOG_LEVEL_NORMAL;
+        m_logLevel = Logger::LOG_LEVEL_NORMAL;
         m_reflectToConsole = false;
         m_maxLogSize = RW_DEFAULT_MAX_LOG_LENGTH;
         m_pFile = (std::fstream*) new std::fstream();
-        m_overflowAction = ACTION_TRUNCATE;
+        m_overflowAction = Logger::ACTION_TRUNCATE;
     }
     
-    Logger::Logger( const std::string& fileName )
+    Logger::Logger(const std::string& logFilePath, const OverflowAction& action)
     {
-        m_path = fileName;
+        m_path = logFilePath;
         m_enabled = true;
-        m_logLevel = LOG_LEVEL_NORMAL;
+        m_logLevel = Logger::LOG_LEVEL_NORMAL;
         m_reflectToConsole = false;
         m_maxLogSize = RW_DEFAULT_MAX_LOG_LENGTH;
         m_pFile = (std::fstream*) new std::fstream();
-        m_overflowAction = ACTION_TRUNCATE;
+        m_overflowAction = action;
     }
     
     Logger::~Logger()
@@ -52,6 +49,79 @@ namespace rw
     void Logger::close()
     {
         if (((std::fstream*)m_pFile)->is_open()) ((std::fstream*)m_pFile)->close();
+    }
+    
+    void Logger::setEnabled( bool enabled )
+    {
+        // RAII locker -> https://en.cppreference.com/w/cpp/language/raii
+        // Any updates to Logger objects should be protected
+        std::lock_guard<std::mutex> lk(m_mutex);
+        m_enabled = enabled;
+    }
+    
+    bool Logger::isEnabled() const
+    {
+        return m_enabled;
+    }
+    
+    void Logger::setReflectToConsole( bool b)
+    {
+        std::lock_guard<std::mutex> lk(m_mutex);
+        m_reflectToConsole = b;
+    }
+    
+    bool Logger::isReflectToConsole( ) const
+    {
+        return m_reflectToConsole;
+    }
+    
+    void Logger::setMaxLogSize( size_t maxLen )
+    {
+        std::lock_guard<std::mutex> lk(m_mutex);
+        if (maxLen<=m_minLogSize) maxLen = m_minLogSize;
+        m_maxLogSize = maxLen;
+    }
+    
+    size_t Logger::getMaxLogSize() const
+    {
+        return m_maxLogSize;
+    }
+    
+    void Logger::setLogLevel( Level level )
+    {
+        std::lock_guard<std::mutex> lk(m_mutex);
+        m_logLevel = level;
+    }
+    
+    Logger::Level Logger::getLogLevel() const
+    {
+        return m_logLevel;
+    }
+    
+    std::string Logger::getPath() const
+    {
+        return m_path;
+    }
+    
+    size_t Logger::getLogSize()
+    {
+        std::lock_guard<std::mutex> lk(m_mutex);
+        if( open() == Logger::RES_OK ) {
+            ((std::fstream*)m_pFile)->seekp(0, std::ios::end );
+            size_t size = (size_t) ((std::fstream*)m_pFile)->tellp();
+            close();
+            return size;
+        }
+        return 0;
+    }
+    
+    Logger::Result Logger::open()
+    {
+        std::lock_guard<std::mutex> lk(m_mutex);
+        if ( ((std::fstream*)m_pFile)->is_open()) return Logger::RES_OK;
+        
+        ((std::fstream*)m_pFile)->open( m_path.c_str(), std::ios::out | std::ios::app  );
+        return ((std::fstream*)m_pFile)->is_open() ? Logger::RES_OK : Logger::RES_FILE_ERROR;
     }
 }
 
--- src/Logger.h
+++ src/Logger.h
@@ -17,26 +17,39 @@
 
 namespace rw
 {
-    enum LogLevel
-    {
-        LOG_LEVEL_ERROR        = -2,
-        LOG_LEVEL_WARNING    = -1,
-        LOG_LEVEL_NORMAL    = 0,
-        LOG_LEVEL_DEBUG        = 1,
-        LOG_LEVEL_INSANE    = 2
-    };
-    
-    enum OverflowAction {
-        ACTION_NONE = 0,
-        ACTION_TRUNCATE = 1,
-        ACTION_ROTATE = 2
-    };
-    
     /**
      * @brief    Thread safe Logger class.
      */
-    class Logger final
+    class Logger
     {
+    public:
+        //Public logger enumerations
+        enum Level
+        {
+            LOG_LEVEL_ERROR        = -2,
+            LOG_LEVEL_WARNING    = -1,
+            LOG_LEVEL_NORMAL    = 0,
+            LOG_LEVEL_DEBUG        = 1,
+            LOG_LEVEL_INSANE    = 2
+        };
+        
+        enum OverflowAction {
+            ACTION_NONE = 0,
+            ACTION_TRUNCATE = 1,
+            ACTION_ROTATE = 2
+        };
+        
+        enum Result {
+            RES_OK = 0,                 ///< Success
+            RES_ERROR,                  ///< Generic error
+            RES_BAD_ARGS,               ///< Some args are invalid
+            RES_INVALID_VALUE,          ///< A value is invalid
+            RES_INVALID_OPERATION,      ///< Operation is not allowed
+            RES_MEMORY_ERROR,           ///< A memory problem occured (most probably out of memory)
+            RES_FILE_ERROR,             ///< A file error occured (most probrably file not found or supported)
+            RES_NULL_POINTER            ///< NULL pointer
+        };
+        
     private:
         std::string             m_path;                         ///< Output file path. In case of empty string, Logger do not write to a file
         void                    *m_pFile;                       ///< Output file. It's opened only when required.
@@ -44,20 +57,75 @@ namespace rw
         bool                    m_enabled;                      ///< Enables/disables logging
         size_t                  m_maxLogSize;                   ///< Approximate max length of log file in bytes.
         std::mutex              m_mutex;                        ///< For locking logging operation in a multi threaded environment
-        LogLevel                m_logLevel;                     ///< Defines the level of importance of the messages, only this and lower level messages are logged.
+        Level                   m_logLevel;                     ///< Defines the level of importance of the messages, only this and lower level messages are logged.
         OverflowAction          m_overflowAction;               ///< Decides what to do when the log size exceeds max log sizes
+        const size_t            m_minLogSize = 512;             ///< Minimum value of maximum log size and minimum size after truncation/rotation
         
     public:
+        
+        //Destructor
         virtual ~Logger();
+        
+        // Getters and setters.
+        void            setEnabled( bool enabled );
+        bool            isEnabled( ) const;
+        void            setReflectToConsole( bool b);
+        bool            isReflectToConsole( ) const;
+        
+        /**
+         * @brief              Sets maximum log size. It does not truncate/rotate the file immediately if the current file size is greater,
+         *                     but new log operation will truncate/rotate the file.
+         * @param    maxLen    The maximum size of log file. If the max log file size is lower than a
+         *                     minimum value, maxLen is set to the minimum allowed size.
+         */
+        void            setMaxLogSize( size_t maxLen );
+        
+        /**
+         * @brief               Gets maximum log size.
+         * @return              The maximum log size.
+         */
+        size_t          getMaxLogSize() const;
+        
+        /**
+         * @brief               Sets the log level.
+         * @param    level      The log level. Only this and lower level messages are logged.
+         */
+        void            setLogLevel( Level level );
+        
+        /**
+         * @brief               Gets log level.
+         * @return              The log level.
+         */
+        Level        getLogLevel() const;
+        
+        /**
+         * @brief               Gets the path to log file. Logger does not keep track any information about truncated or rotated logs.
+         Therefore, this path is the initialized path that the object is logging
+         * @return              The log file path.
+         */
+        std::string        getPath() const;
+        
+        /**
+         * @brief               Gets the current size of the log file. Logger does not keep track any information about truncated or rotated logs.
+                                Therefore, this size is the size remaining of the file after any number truncations or rotations
+         * @return              The current size of the log file.
+         */
+        size_t            getLogSize();
         
     private:
         //Cannot instantiate object outside Logger class
-        Logger( const std::string& logFilePath );
+        Logger(const std::string& logFilePath, const OverflowAction& action);
         Logger();
         Logger(const Logger& other);
         
         /**
-         * @brief    Closes m_pFile (does not delete, only closes).
+         * @brief               Opens m_pFile (does not create, only opens).
+         * @return              RESULT_OK if successful.
+         */
+        Result          open();
+        
+        /**
+         * @brief               Closes m_pFile (does not delete, only closes).
          */
         void            close();
     };
