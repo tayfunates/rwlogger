339e4f6e1aaacf306a33925b15c853949bb26d58
Tayfun Ateş <tayfun.ates@avcrtech.com>
2019-02-09 17:37:22 +0300

    Some unit tests are added.

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,96 @@
 
 #include <iostream>
 #include "Logger.h"
+#include <fstream>
 
 using namespace rw;
 
+void TEST_init()
+{
+    auto res = Logger::init();
+    assert (res == Logger::RES_OK);
+}
+
+void TEST_getAndDestroyLoggers()
+{
+    auto consoleLogger = Logger::getConsoleLogger();
+    assert(consoleLogger);
+    auto defaultLogger = Logger::getDefaultLogger();
+    assert(defaultLogger);
+    auto customLogger = Logger::getFileLogger("customLogger.log");
+    assert(customLogger);
+    
+    //Removing default logger should be impossible
+    assert(Logger::destroy("rw_default_log.txt") == Logger::RES_BAD_ARGS);
+    
+    //Removing console logger should be impossibe
+    assert(Logger::destroy("") == Logger::RES_BAD_ARGS);
+    
+    //Custom file loggers can be removed from our container
+    assert(Logger::destroy("customLogger.log") == Logger::RES_OK);
+    //Retrieving again should not be a problem
+    customLogger = Logger::getFileLogger("customLogger.log");
+    assert(customLogger);
+    //Keep it clean
+    assert(Logger::destroy("customLogger.log") == Logger::RES_OK);
+    
+    assert(Logger::destroy("thisLoggerIsNotInTheContainer.log") == Logger::RES_ERROR);
+    
+    remove("customLogger.log");
+    remove("rw_default_log.txt");
+}
+
+size_t getFileSize(const std::string& filePath)
+{
+    std::fstream inFile;
+    inFile.open( filePath.c_str(), std::ios::in);
+    if( inFile.is_open() ) {
+        inFile.seekg(0, std::ios::end );
+        size_t size = (size_t) inFile.tellg();
+        inFile.close();
+        return size;
+    }
+    return 0;
+}
+
+void TEST_enableDisable()
+{
+    const std::string testFile = "TEST_enableDisable.log";
+    const std::string errorMessage = "Error Occured";
+    
+    auto customLogger = Logger::getFileLogger(testFile);
+    customLogger->setEnabled(false);
+    customLogger->operator()(Logger::LOG_LEVEL_ERROR) << errorMessage;
+    assert(getFileSize(testFile) == 0);
+    customLogger->setEnabled(true);
+    customLogger->operator()(Logger::LOG_LEVEL_ERROR) << errorMessage;
+    assert(getFileSize(testFile) > 0);
+    
+    remove(testFile.c_str());
+}
+
+void TEST_logLevel()
+{
+    const std::string testFile = "TEST_logLevel.log";
+    const std::string warningMessage = "Warning Occured";
+    
+    auto customLogger = Logger::getFileLogger(testFile);
+    customLogger->setLogLevel(Logger::LOG_LEVEL_ERROR);
+    customLogger->operator()(Logger::LOG_LEVEL_WARNING) << warningMessage;
+    assert(getFileSize(testFile) == 0);
+    customLogger->setLogLevel(Logger::LOG_LEVEL_WARNING);
+    customLogger->operator()(Logger::LOG_LEVEL_WARNING) << warningMessage;
+    assert(getFileSize(testFile) > 0);
+    
+    remove(testFile.c_str());
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    LOGC(Logger::LOG_LEVEL_WARNING) << " Hello World!" << " and a integer " <<  17;
-    LOGD(Logger::LOG_LEVEL_WARNING) << " Hello World!" << " and a integer " <<  17;
-    LOGF(Logger::LOG_LEVEL_WARNING, "Deneme.txt") << " Hello World!" << " and a integer " <<  17;
+    
+    TEST_init();
+    TEST_getAndDestroyLoggers();
+    TEST_enableDisable();
+    TEST_logLevel();
+    
     return 0;
 }
